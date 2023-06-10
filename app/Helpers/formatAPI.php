959d8d1c4ec938e36f2ff50eb2ec436b4ea586c2
alezafazriliyadisudrajat <alezafazriliyadisudrajat@smkwikrama.sch.id>
2023-06-10 22:06:34 +0700

    update

--- app/Helpers/formatAPI.php
+++ app/Helpers/formatAPI.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Helpers;
-use App\Models\apotek;
+use App\Models\Apotek;
 
 class formatApi{
 
--- app/Http/Controllers/ApotekController.php
+++ app/Http/Controllers/ApotekController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\apotek;
+use App\Models\Apotek;
 use App\Helpers\formatAPI;
 use Illuminate\Http\Request;
 use Exception;
@@ -24,9 +24,9 @@ class ApotekController extends Controller
     public function store(Request $request)
     {
         try{
-            $apotek = apotek::create($request->all());
+            $apotek = Apotek::create($request->all());
 
-            $data = apotek::where('id','=',$apotek->id)->get();
+            $data = Apotek::where('id','=',$apotek->id)->get();
             if($data){
                 return formatAPI::createAPI(200, 'berhasil', $data);
              }else{
@@ -40,7 +40,7 @@ class ApotekController extends Controller
     public function show($id)
     {
         try{
-            $data = apotek::where('id', '=',$id)->first();
+            $data = Apotek::where('id', '=',$id)->first();
             if($data){
                 return formatAPI::createAPI(200, 'berhasil', $data);
              }else{
@@ -53,19 +53,44 @@ class ApotekController extends Controller
     public function update(Request $request, $id)
     {
         try{
-            $apotek = apotek::findorfail($id);
-            $apotek->update($request->all());
 
-            $data = apotek::where('id','=',$apotek->id)->get();
-            if($data){
-                return formatAPI::createAPI(200, 'berhasil', $data);
-             }else{
-                return formatAPI::createAPI(400, 'Failed');
+            $request->validate([
+                'nama' => 'required',
+                'rujukan' => 'required',
+                'rumah_sakit' => 'required',
+                'obat' => 'required',
+                'harga_satuan' => 'required',
+                'total_harga' => 'required',
+                'apoteker' => 'required',
+            ]);
+
+            $obat = preg_split("/[,]", $request->obat);
+            $harga = array_map('intval', explode(',', $request->harga_satuan));
+
+            $apotek = Apotek::create([
+                'nama' => $request->nama,
+                'rujukan' => $request->rujukan,
+                'rumah_sakit' => $request->rumah_sakit,
+                'obat' => $obat,
+                'harga_Satuan' => $harga,
+                'total_harga' => $request->total_harga,
+                'apoteker' => $request->apoteker,
+            ]);
+
+            $getDataSaved = Apotek::where('id', '=', $apotek->id)->first();
+
+            if($getDataSaved) {
+                return formatAPI::createAPI(200, 'Berhasil', $getDataSaved);
+            }else{
+                return formatAPI::createAPI(400, 'Gagal', );
             }
 
+            
         }catch(Exception $error){
             return formatAPI::createAPI(400, 'Gagal',$error);
         }
+
+        
     }
     
     public function destroy($id){
