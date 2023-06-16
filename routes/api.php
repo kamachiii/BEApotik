1564368a27b7ac9dfb22be95e18819ad35763b92
kamachiii <hengkert3jy@gmail.com>
2023-06-16 11:03:55 +0700

    create rest api for rental and sampah

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ApotekController;
+use App\Http\Controllers\RentalController;
+use App\Http\Controllers\SampahController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -31,4 +33,35 @@ Route::prefix('apotek')->group(function () {
     });
 });
 
+//* Rest API routes for Rental
+Route::prefix('rental')->group(function () {
+    Route::get('/',[RentalController::class, 'index']);
+    Route::post('/create',[RentalController::class, 'store']);
+    Route::get('/show/{id}',[RentalController::class, 'show']);
+    Route::post('/update/{id}',[RentalController::class, 'update']);
+    Route::post('/delete/{id}',[RentalController::class, 'destroy']);
+
+    Route::prefix('trash')->group(function () {
+        Route::get('/all', [RentalController::class, 'getTrash']);
+        Route::post('/restore/{id}', [RentalController::class, 'restore']);
+        Route::post('/permanent/{id}', [RentalController::class, 'deleteTrash']);
+
+    });
+});
+
+//* Rest API routes for Sampah
+Route::prefix('sampah')->group(function () {
+    Route::get('/',[SampahController::class, 'index']);
+    Route::post('/create',[SampahController::class, 'store']);
+    Route::get('/show/{id}',[SampahController::class, 'show']);
+    Route::post('/update/{id}',[SampahController::class, 'update']);
+    Route::post('/delete/{id}',[SampahController::class, 'destroy']);
+
+    Route::prefix('trash')->group(function () {
+        Route::get('/all', [SampahController::class, 'getTrash']);
+        Route::post('/restore/{id}', [SampahController::class, 'restore']);
+        Route::post('/permanent/{id}', [SampahController::class, 'deleteTrash']);
+
+    });
+});
 
